19b98a326c565b9841a87adb8a61680593905fb0
WavuaW <wavuamuroka@gmail.com>
2023-04-27 20:09:41 +0300

    advanced

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
-'''Task 11: More involved type annotations
+'''Task 10:  Duck typing - first element of a sequence
 '''
-from typing import Any, Mapping, Union, TypeVar
+from typing import Any, Sequence, Union
 
 
-T = TypeVar('T')
-Res = Union[Any, T]
-Def = Union[T, None]
-
-
-def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
-    #Retrieves a value from a dict using a given key.
-    if key in dct:
-        return dct[key]
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''Retrieves the first element of a sequence if it exists.
+    '''
+    if lst:
+        return lst[0]
     else:
-        return default
+        return None
      
